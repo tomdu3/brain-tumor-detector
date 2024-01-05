2cf1b2d93413e040d2515a39116191af9b99e7be
Tomislav Dukez <tomdu3@ymail.com>
2024-01-05 15:45:33 +0000

    Add quick summary page template

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
-
+from app_pages.page_summary import page_summary_body
 
 app = MultiPage(app_name="Brain Tumor Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
-
-
+app.add_page("Quick Project Summary", page_summary_body)
 app.run()  # Run the app
---
+++ app_pages/page_summary.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_summary_body():
+
+    st.write('### Quick Project Summary')
+
+    st.info(
+        f'**General Information**\n'
+        f'* Generally about the brain tumor\n'
+        f'* Different causes.\n'
+        )
+
+    st.write(
+        f'* For additional information, please visit and **read** the '
+        f'[Project README file](https://github.com/tomdu3/brain-tumor-detector).')
+    
+
+    st.success(
+        f'The project has 2 business requirements:\n'
+        f'* 1 - The client would like to have a study of the dataset collected at the '
+        f'Department of Brain Repair and Rehabilitation\n' 
+        f'* 2 - The client would like to have a ML model developed in order to be able to identify the brain tumor  from the MRI scan.'
+        )
