da9928cb1bbb32e8ef86e93db4e67dc5c24327bf
Tomislav Dukez <tomdu3@ymail.com>
2024-01-06 09:16:48 +0000

    Add mri visualizer streamlit page

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_mri_visualizer import page_mri_visualizer_body
 
-app = MultiPage(app_name="Brain Tumor Detector")  # Create an instance of the app
+
+app = MultiPage(app_name='Brain Tumor Detector')  # Create an instance of the app
 
 # Add your app pages here using .add_page()
-app.add_page("Quick Project Summary", page_summary_body)
+app.add_page('Quick Project Summary', page_summary_body)
+app.add_page('MRI Visualizer', page_mri_visualizer_body)
 app.run()  # Run the app
